45987dd1d78688673ab614ed999fd8a435bf2d87
echamorror01 <echamorror01@suarezdefigueroa.es>
2025-09-30 20:39:34 +0200

    cambios

---
+++ Arrays/Ejercicio1.js
@@ -0,0 +1,17 @@
+/*
+1. Crea un programa que pida al usuario 6 números y luego los muestre en orden inverso (pista: usa
+un array para almacenarlos y "for" para mostrarlos).*/
+
+let num;
+let miarray = new Array();
+let contador = 0;
+
+do {
+  num = parseInt(prompt("Introduce un numero"));
+  miarray.push(num); //aqui vamos guardando todos los numeros y lo vamos almacenando
+  contador++;
+} while (contador != 6);
+
+for (let i = miarray.length - 1; i >= 0; i--) {
+  console.log("El elemento " + i + " toma el valor " + miarray[i]);
+}
