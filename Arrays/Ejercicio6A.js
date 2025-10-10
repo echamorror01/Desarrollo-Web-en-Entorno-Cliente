c277a2164766bbbc267ff7726c269028decc56e8
echamorror01 <echamorror01@suarezdefigueroa.es>
2025-10-10 19:09:42 +0200

    ok

---
+++ Arrays/Ejercicio6A.js
@@ -0,0 +1,20 @@
+function creartabla() {
+  let tamaño = parseInt(prompt("Introduce el tamaño de la matriz cuadrada"));
+  let resultado = "<table border=1 ";
+  for (let filas = 1; filas <= tamaño; filas++) {
+    resultado += "<tr>";
+    for (let columnas = 1; columnas <= tamaño; columnas++) {
+      let valor = parseInt(
+        prompt(
+          "Introduce el valo9r para la fila " +
+            filas +
+            " y la columna " +
+            columnas
+        )
+      );
+      resultado += "<td>" + valor + "</td>";
+    }
+    resultado += "</tr>";
+  }
+  resultado += "</table>";
+}
--- Arrays/Ejercicio8.js
+++ Arrays/Ejercicio8.js
@@ -1,17 +1,15 @@
-//7. Dado un array con elementos repetidos, escribe el código para que saber cuál es el elemento que
-//más veces aparece en el array.
-
-// los array asociativos tienen clave y valor , contador obejto y elemento es la clave
-//contador[3]=5 el numero 3 aparecio 5 veces
-
+//Modificar el ejercicio anterior, de tal forma que muestra en la consola el número de veces que
+//aparece cada item en el array, pero ordenamos de mayor a menor
 const miarray = ["verde", "amarillo", "verde", "rojo", "verde", "rojo", "rosa"];
 let repeticiones = []; //array asociativo
+let numeroelementos = 0;
 
 for (const valor of miarray) {
   // para buscar las veces que se repite
   // contenido de dentro
   if (repeticiones[valor] == undefined) {
     repeticiones[valor] = 1;
+    numeroelementos++;
   } else {
     repeticiones[valor] += 1;
   }
@@ -40,3 +38,23 @@ alert(
     valormax +
     " repeticiones"
 );
+
+let repeticionesordenado = [];
+
+while (numeroelementos > 0) {
+  let valormax = 0;
+  let indicemax = "";
+  for (const indice in repeticiones) {
+    if (repeticiones[indice] > valormax) {
+      valormax = repeticiones[indice];
+      indicemax = indice; // indice es el verde, aszul
+    }
+  }
+  repeticionesordenado[indicemax] = valormax;
+  delete repeticiones[indicemax];
+  numeroelementos--;
+}
+
+for (const indice in repeticionesordenado) {
+  alert("Hay " + repeticionesordenado[indice] + "elementos de color " + indice);
+}
