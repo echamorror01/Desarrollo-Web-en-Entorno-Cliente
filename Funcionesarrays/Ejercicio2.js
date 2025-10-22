bb144024bae3884e05c11f1bc4f02bc880878e97
echamorror01 <echamorror01@suarezdefigueroa.es>
2025-10-22 22:17:24 +0200

    ok

---
+++ Funcionesarrays/Ejercicio2.js
@@ -0,0 +1,7 @@
+let array = [1, 2, 3, 4, 5, 6];
+let tamaño = array.length;
+let rdo = " ";
+for (let i = 0; i < tamaño; i++) {
+  rdo += array.pop(); //para que salgan todos seguidos
+  alert(rdo);
+}
