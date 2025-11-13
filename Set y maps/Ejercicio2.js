a6bcb90155e25fa0416ab99222c825729a9315bd
echamorror01 <echamorror01@suarezdefigueroa.es>
2025-11-13 10:35:21 +0100

    cambios

--- Set y maps/Ejercicio2.js
+++ Set y maps/Ejercicio2.js
@@ -3,12 +3,14 @@ Hacer una función que reciba un array como parámetro y devuelva true si todos 
 son únicos y false si hay algún elemento repetido. Al pulsar el botón del ejercicio debe mostrar un alert
 indicando "Todos los elementos son únicos" o "Hay elementos repetidos" */
 
-function miarray() {
-  let conjuntoarray = new Set(miarray);
+function miarray(array) {
+  let conjuntoarray = new Set(array);
   return conjuntoarray.size == miarray.length;
 }
+
 function miarrayp() {
-  let miarray = [5, 3, 2, 5, 5, 9, 4, 5];
-  let sonunicos = miarray(miarray);
+  let array = [5, 3, 2, 5, 5, 9, 4, 5];
+  let sonunicos = miarray(array);
   alert(sonunicos ? "Todos son unicos" : "Hay repetidos");
+  //condicion? valor si es vd: valor si es falso
 }
---
+++ Set y maps/Ejercicio3.js
@@ -0,0 +1,24 @@
+/*Ejercicio 3.Tenemos un array con los votos de unas votaciones Nacionales, en cada casilla del array está la provincia, 
+dos puntos y sus votos. Ejemplo: ["Madrid: 125", "Barcelona: 89", "Valencia: 45", "Sevilla: 32","Madrid: 
+31"]. Por un error se han duplicado las provincias en vez de sumarse los votos (pertenecen a varias 
+localidades) y queremos saber cuáles son las provincias que han votado. Hacer una función que reciba el 
+array de votos y devuelva un array con las provincias que han votado. Al pulsar el botón del ejercicio 
+debe mostrar un alert con las provincias que han votado cada una en una línea:  
+"Han votado las provincias:  */
+
+function provincia() {
+  let array = [
+    "Madrid: 125",
+    "Barcelona: 89",
+    "Valencia: 45",
+    "Sevilla: 32",
+    "Madrid: 31",
+  ];
+  let conjunto = new Set();
+  for (const ciudades of array) {
+    let provincia = ciudades.split(":")[0]; //cogemos las provincias
+    conjunto.add(provincia);
+  }
+  let final = Array.from(conjunto);
+  alert("Las provincias que han votado son:\n " + final.join("\n"));
+}
