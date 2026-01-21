6a2d1e852940ac8841f3000f402aefc7e15d1cc8
echamorror01 <echamorror01@suarezdefigueroa.es>
2026-01-21 19:15:24 +0100

    ok

---
+++ 2Evaluacion/DOM/Ejercicio11/Ejercicio11.js
@@ -0,0 +1,17 @@
+/*Ejercicio 11 – Lista dinámica
+Vamos a crear una aplicación que permite añadir y eliminar elementos de una lista.
+Estilo y elementos HTML:
+Recorrer el array de tareas iniciales y crea elementos <li> y dentro <span> con el texto
+de cada tarea, añade un botón eliminar con la clase “btn-eliminar”. Crea una función
+“crearElementoLista” que tenga como parámetro de entrada un texto y devuelva el <li>
+a insertar: */
+
+const tareasIniciales = [
+  "Estudiar JavaScript",
+  "Practicar DOM",
+  "Hacer ejercicios",
+];
+
+tareasIniciales.forEach(function (elementos, indice) {});
+
+function crearElementoLista() {}
