19d94181ec08717e2c296f6548853804566b170f
echamorror01 <echamorror01@suarezdefigueroa.es>
2026-02-14 16:59:28 +0100

    ok

--- 2Evaluacion/DOM/Alumnos/miscript.js
+++ 2Evaluacion/DOM/Alumnos/miscript.js
@@ -1,21 +1,54 @@
-const añadir = document.querySelector("#btnAñadir");
+//Variables
+const anadir = document.querySelector("#btnAnadir");
 const input = document.querySelector("#inputTarea");
+const select = document.querySelector("#selectTipo");
+const template = document.querySelector("#templateTarea");
+const listatareas = document.querySelector("#listaTareas");
+
+//Cosas que necesitamos
 let tareas = [];
+let contador = 0;
 
 function añadirTarea() {
+  if (input.value == "") {
+    alert("Está vacío");
+    return;
+  }
   contador++;
   let nuevaTarea = {
     id: contador,
-    descripcion: "",
-    tipo: "",
+    descripcion: input.value, //los input siempre con value
+    tipo: select.value,
     completada: false,
     fechaCompletada: null,
   };
+  tareas.push(nuevaTarea);
 
-  añadir.addEventListener("click", () => {
-    tareas.push(nuevaTarea);
+  input.value = "";
+  console.log(tareas);
+}
+anadir.addEventListener("click", () => {
+  añadirTarea();
+});
+
+function dibujarTareas() {
+  listatareas.innerHTML = "";
+
+  tareas.forEach(function (tarea, indice) {
+    const clon = template.content.cloneNode(true);
+    const li = clon.querySelector("li");
+    const descripcion = clon.querySelector(".tarea-desc");
+    descripcion.textContent = tarea.descripcion;
+    const tipo = clon.querySelector(".tarea-tipo");
+    tipo.textContent = tarea.tipo;
+    const fecha = clon.querySelector(".tarea-fecha");
+    fecha.textContent = tarea.fechaCompletada;
+    const completar = clon.querySelector(".btn-completar");
+    const eliminar = clon.querySelector(".btn-eliminar");
+
+    if (tarea.fechaCompletada == "") {
+      fecha.remove();
+      return;
+    }
   });
-  if ((input.textContent = "")) {
-    alert("Está vacío");
-  }
 }
